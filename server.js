4557082291f6398c34923356acbb70d2b771d86a
IvanB56 <burack.iw@yandex.ru>
2022-12-27 20:14:53 +0300

    fix server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`сервер доступен по адресу http://localhost:${PORT}`);
 });
