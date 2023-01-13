babcd6d3c44294fea79a8b92b9c85d8aad4cd29e
IvanB56 <burack.iw@yandex.ru>
2023-01-13 21:56:30 +0300

    Add Router and Store

--- src/pages/FormRegistration/registrationAPI.ts
+++ src/pages/FormRegistration/registrationAPI.ts
@@ -1,7 +1,7 @@
 import {Fetch} from "core";
 
 export const registrationApi = {
-    registration(data: RegistrationRequest) {
+    registration(data: string) {
         return Fetch.post('https://ya-praktikum.tech/api/v2/auth/signup', {data: data}).then(r => JSON.parse(r.responseText));
     }
 }
